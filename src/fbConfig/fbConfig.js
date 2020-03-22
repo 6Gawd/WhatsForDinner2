2ba11930243b52cf3a8e0c942be0d3cf017288b7
Yan <ing.yangao@gmail.com>
2020-03-22 09:26:39 -0400

    refactored db

--- src/fbConfig/fbConfig.js
+++ src/fbConfig/fbConfig.js
@@ -4,19 +4,22 @@ import 'firebase/analytics';
 import 'firebase/auth';
 
 export const firebaseConfig = {
-  apiKey: 'AIzaSyD6vA7rr68uDCsGdIwoKMj6QPflFDK_deE',
-  authDomain: 'whatsfordinner-a595e.firebaseapp.com',
-  databaseURL: 'https://whatsfordinner-a595e.firebaseio.com',
-  projectId: 'whatsfordinner-a595e',
-  storageBucket: 'whatsfordinner-a595e.appspot.com',
-  messagingSenderId: '236985381389',
-  appId: '1:236985381389:web:f554fef55a4350face5b44',
-  measurementId: 'G-VRG26JJQWZ'
+	apiKey: 'AIzaSyD6vA7rr68uDCsGdIwoKMj6QPflFDK_deE',
+	authDomain: 'whatsfordinner-a595e.firebaseapp.com',
+	databaseURL: 'https://whatsfordinner-a595e.firebaseio.com',
+	projectId: 'whatsfordinner-a595e',
+	storageBucket: 'whatsfordinner-a595e.appspot.com',
+	messagingSenderId: '236985381389',
+	appId: '1:236985381389:web:f554fef55a4350face5b44',
+	measurementId: 'G-VRG26JJQWZ'
 };
 
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
 firebase.firestore();
+
+export const auth = firebase.auth();
+export const db = firebase.firestore();
 
 export default firebase;
 // export function getCurrentUser() {
