0e414727b35c20185be585673eb8470304997964
Mike-Damato <damatomike@gmail.com>
2020-03-22 12:36:19 -0400

    Add- Add delete ingredient reducer & add ingredient reducer to list component

--- src/components/List.js
+++ src/components/List.js
@@ -1,21 +1,87 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import {
+  addIngredient,
+  deleteIngredient
+} from '../store/redux/ingredientsReducer';
 
 export class List extends Component {
-	render() {
-		return (
-			<div>
-				<h1>LIST</h1>
-				<ul>{this.props.ingredients.map((ingredient) => <li key={ingredient.name}>{ingredient.name}</li>)}</ul>
-			</div>
-		);
-	}
+  render() {
+    //====================MIKE's REFACTOR======================//
+    const { ingredients } = this.props;
+    const ingredientList = ingredients.length ? (
+      ingredients.map(ingredient => {
+        return (
+          <div key={ingredient.name}>
+            <p>{ingredient.name}</p>
+            <div className="col s12 l6 right">
+              <button
+                className="btn-floating waves-effect waves-light red right"
+                type="button"
+                name="action"
+                onClick={() => this.props.deleteIngredient(ingredient)}
+              >
+                <i className="material-icons">x</i>
+              </button>
+            </div>
+          </div>
+        );
+      })
+    ) : (
+      <p>No Ingredients Yet</p>
+    );
+    //====================END MIKE's REFACTOR===============//
+
+    //====================YAN'S ORIGINAL CODE===============//
+    /* <ul>
+          {this.props.ingredients.map(ingredient => (
+            <li key={ingredient.name}>{ingredient.name}</li>
+          ))}
+        </ul> */
+    //===============END YAN'S CODE=======================//
+
+    return (
+      // INGREDIENT LIST FORM
+      <div>
+        <h1>Your Shopping List</h1>
+        {ingredientList}
+
+        {/* INPUT ITEM FORM */}
+        <div className="main-header">
+          <div className="showcase container">
+            <div className="row">
+              <div className="col s12 m10 offset-m1 center">
+                <label htmlFor="ingredient">Add Ingredient</label>
+                <input
+                  type="text"
+                  name="ingredient"
+                  onChange={this.handleChange}
+                />
+              </div>
+              <button
+                className="btn waves-effect waves-light indigo center"
+                type="button"
+                name="action"
+              >
+                Add Ingredient
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-const mapStateToProps = (state) => ({
-	ingredients: state.ingredients
+const mapStateToProps = state => ({
+  ingredients: state.ingredients
 });
 
-const mapDispatchToProps = {};
+const mapDispatchToProps = dispatch => ({
+  addIngredient: (ingredient, userId) =>
+    dispatch(addIngredient(ingredient, userId)),
+  deleteIngredient: (ingredient, userId) =>
+    dispatch(deleteIngredient(ingredient, userId))
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(List);
--- src/store/redux/ingredientsReducer.js
+++ src/store/redux/ingredientsReducer.js
@@ -2,59 +2,90 @@ import { auth, db } from '../../fbConfig/fbConfig';
 
 const GOT_INGREDIENTS = 'GOT_INGREDIENTS';
 const ADDED_INGREDIENT = 'ADDED_INGREDIENT';
+const DELETED_INGREDIENT = 'DELETED_INGREDIENT';
 
-const addedIngredient = (ingredient) => {
-	return {
-		type: ADDED_INGREDIENT,
-		ingredient
-	};
+const addedIngredient = ingredient => {
+  return {
+    type: ADDED_INGREDIENT,
+    ingredient
+  };
 };
 
-const gotIngredients = (ingredients) => {
-	return {
-		type: GOT_INGREDIENTS,
-		ingredients
-	};
+const gotIngredients = ingredients => {
+  return {
+    type: GOT_INGREDIENTS,
+    ingredients
+  };
+};
+
+const deletedIngredient = ingredient => {
+  return {
+    type: DELETED_INGREDIENT,
+    ingredient
+  };
 };
 
 export const addIngredient = (ingredient, userId) => {
-	return async (dispatch) => {
-		try {
-			const newIngredient = await db.collection('ingredients').doc(userId).set({
-				name: ingredient.name
-			});
-			dispatch(gotIngredients(newIngredient));
-		} catch (error) {
-			console.log('No Ingredients', error);
-		}
-	};
+  return async dispatch => {
+    try {
+      const newIngredient = await db
+        .collection('ingredients')
+        .doc(userId)
+        .set({
+          name: ingredient.name
+        });
+      dispatch(gotIngredients(newIngredient));
+    } catch (error) {
+      console.log('No Ingredients', error);
+    }
+  };
 };
 
-export const getIngredients = (userId) => {
-	return async (dispatch) => {
-		try {
-			const ingredients = [];
-			await db.collection('ingredients').where('userId', '==', userId).get().then(function(querySnapshot) {
-				querySnapshot.forEach(function(doc) {
-					ingredients.push(doc.data());
-				});
-			});
-			dispatch(gotIngredients(ingredients));
-		} catch (error) {
-			console.log('No Ingredients', error);
-		}
-	};
+export const getIngredients = userId => {
+  return async dispatch => {
+    try {
+      const ingredients = [];
+      await db
+        .collection('ingredients')
+        .where('userId', '==', userId)
+        .get()
+        .then(function(querySnapshot) {
+          querySnapshot.forEach(function(doc) {
+            ingredients.push(doc.data());
+          });
+        });
+      dispatch(gotIngredients(ingredients));
+    } catch (error) {
+      console.log('No Ingredients', error);
+    }
+  };
+};
+
+export const deleteIngredient = (ingredient, userId) => {
+  return async dispatch => {
+    try {
+      console.log('DELETE INGREDIENT THUNK');
+      await db
+        .collection('ingredients')
+        .where('userId', '==', userId)
+        .remove();
+    } catch (error) {
+      console.error('Error deleting ingredient', error);
+    }
+  };
 };
 
 const ingredientsReducer = (state = [], action) => {
-	switch (action.type) {
-		case GOT_INGREDIENTS:
-			return action.ingredients;
-		case ADDED_INGREDIENT:
-			return [ ...state, action.ingredient ];
-		default:
-			return state;
-	}
+  switch (action.type) {
+    case GOT_INGREDIENTS:
+      return action.ingredients;
+    case ADDED_INGREDIENT:
+      return [...state, action.ingredient];
+    case DELETED_INGREDIENT:
+      return state.filter(ingredient => ingredient.id !== action.ingredient);
+    default:
+      return state;
+  }
 };
 
 export default ingredientsReducer;
