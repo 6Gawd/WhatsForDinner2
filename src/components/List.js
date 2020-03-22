5f68b0d3931cad78a9fc8457cb43c3ba1a2280e4
Yan <ing.yangao@gmail.com>
2020-03-22 18:38:12 -0400

    Fix- speech bug

--- src/components/List.js
+++ src/components/List.js
@@ -1,112 +1,102 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import {
-  addedIngredient,
-  deletedIngredient
-} from '../store/redux/ingredientsReducer';
+import { addedIngredient, deletedIngredient } from '../store/redux/ingredientsReducer';
 import { Redirect } from 'react-router-dom';
 import OutputSpeech from './Speech/OutputSpeech';
 import InputSpeech from './Speech/InputSpeech';
 
 export class List extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: '',
-      userId: props.auth.uid
-    };
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			name: '',
+			userId: props.auth.uid
+		};
+	}
 
-  handleChange = event => {
-    this.setState({
-      [event.target.name]: event.target.value
-    });
-  };
+	handleChange = (event) => {
+		this.setState({
+			[event.target.name]: event.target.value
+		});
+	};
 
-  handleSubmit = event => {
-    event.preventDefault();
-    // this.playback(this.state.name);
-    this.props.addIngredient(this.state);
-    this.setState({ name: '' });
-  };
+	handleSubmit = (event) => {
+		event.preventDefault();
+		// this.playback(this.state.name);
+		this.props.addIngredient(this.state);
+		this.setState({ name: '' });
+	};
 
-  startedListening = name => {
-    this.setState({ name: name });
-  };
+	startedListening = (name) => {
+		this.setState({ name: name });
+	};
 
-  render() {
-    const { auth } = this.props;
-    if (!auth.uid) return <Redirect to="/login" />;
-    console.log('LIST PROPS', this.props);
-    const { ingredients } = this.props;
-    const ingredientList = ingredients.length ? (
-      ingredients.map(ingredient => {
-        return (
-          <div key={ingredient.id}>
-            <p>{ingredient.name}</p>
-            <button
-              className="btn-small waves-effect waves-light red right"
-              type="button"
-              name="action"
-              onClick={() => this.props.deleteIngredient(ingredient.id)}
-            >
-              <i className="material-icons">x</i>
-            </button>
-          </div>
-        );
-      })
-    ) : (
-      <p>No Ingredients Yet</p>
-    );
-    return (
-      // INGREDIENT LIST FORM
-      <div>
-        <h1 className="center-align">Your Shopping List</h1>
-        {ingredientList}
+	render() {
+		const { auth } = this.props;
+		if (!auth.uid) return <Redirect to="/login" />;
+		console.log('LIST PROPS', this.props);
+		const { ingredients } = this.props;
+		const ingredientList = ingredients.length ? (
+			ingredients.map((ingredient) => {
+				return (
+					<div key={ingredient.id}>
+						<p>{ingredient.name}</p>
+						<button
+							className="btn-small waves-effect waves-light red right"
+							type="button"
+							name="action"
+							onClick={() => this.props.deleteIngredient(ingredient.id)}
+						>
+							<i className="material-icons">x</i>
+						</button>
+					</div>
+				);
+			})
+		) : (
+			<p>No Ingredients Yet</p>
+		);
+		return (
+			// INGREDIENT LIST FORM
+			<div>
+				<h1 className="center-align">Your Shopping List</h1>
+				{ingredientList}
 
-        {/* INPUT ITEM FORM */}
-        <form onSubmit={this.handleSubmit}>
-          <div className="main-header">
-            <div className="showcase container">
-              <div className="row">
-                <div className="col s12 m10 offset-m1 center">
-                  <label htmlFor="ingredient">Add Ingredient</label>
-                  <input
-                    value={this.state.name}
-                    name="name"
-                    onChange={this.handleChange}
-                  />
-                </div>
-                <button
-                  className="btn waves-effect waves-light indigo center"
-                  type="submit"
-                  name="action"
-                >
-                  Add Ingredient
-                </button>
-                <OutputSpeech
-                  content={this.state.name}
-                  onClick={() => this.handleSubmit}
-                ></OutputSpeech>
-                <InputSpeech startedListening={this.startedListening} />
-              </div>
-            </div>
-          </div>
-        </form>
-      </div>
-    );
-  }
+				{/* INPUT ITEM FORM */}
+				<form onSubmit={this.handleSubmit}>
+					<div className="main-header">
+						<div className="showcase container">
+							<div className="row">
+								<div className="col s12 m10 offset-m1 center">
+									<label htmlFor="ingredient">Add Ingredient</label>
+									<input value={this.state.name} name="name" onChange={this.handleChange} />
+								</div>
+								<button
+									className="btn waves-effect waves-light indigo center"
+									type="submit"
+									name="action"
+								>
+									Add Ingredient
+								</button>
+								<OutputSpeech content={this.state.name} onClick={() => this.handleSubmit} />
+							</div>
+						</div>
+					</div>
+				</form>
+				<InputSpeech startedListening={this.startedListening} />
+			</div>
+		);
+	}
 }
 
-const mapStateToProps = state => ({
-  ingredients: state.ingredients,
-  auth: state.firebase.auth,
-  test: state
+const mapStateToProps = (state) => ({
+	ingredients: state.ingredients,
+	auth: state.firebase.auth,
+	test: state
 });
 
-const mapDispatchToProps = dispatch => ({
-  addIngredient: ingredient => dispatch(addedIngredient(ingredient)),
-  deleteIngredient: id => dispatch(deletedIngredient(id))
+const mapDispatchToProps = (dispatch) => ({
+	addIngredient: (ingredient) => dispatch(addedIngredient(ingredient)),
+	deleteIngredient: (id) => dispatch(deletedIngredient(id))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(List);
--- src/components/Speech/InputSpeech.js
+++ src/components/Speech/InputSpeech.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 
-const SpeechRecognition =
-  window.SpeechRecognition || window.webkitSpeechRecognition;
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 const recognition = new SpeechRecognition();
 
 recognition.continous = true;
@@ -9,57 +8,60 @@ recognition.interimResults = true;
 recognition.lang = 'en-US';
 
 class InputSpeech extends Component {
-  constructor() {
-    super();
-    this.state = {
-      name: '',
-      listening: false
-    };
-  }
+	constructor() {
+		super();
+		this.state = {
+			name: '',
+			listening: false
+		};
+	}
 
-  toggleListen = () => {
-    this.setState(
-      {
-        listening: !this.state.listening
-      },
-      this.handleListen()
-    );
-  };
+	toggleListen = () => {
+		this.setState(
+			{
+				listening: !this.state.listening
+			},
+			this.handleListen
+		);
+	};
 
-  handleListen = () => {
-    console.log('BEFORE LISTENING');
-    if (this.state.listening) recognition.start();
-    console.log('started listening!');
-    let finalTranscript = '';
-    recognition.onresult = event => {
-      let interimTranscript = '';
+	handleListen = () => {
+		console.log('BEFORE LISTENING');
+		if (this.state.listening) recognition.start();
+		console.log('started listening!');
+		let finalTranscript = '';
+		recognition.onresult = (event) => {
+			let interimTranscript = '';
 
-      for (let i = event.resultIndex; i < event.results.length; i++) {
-        const transcript = event.results[i][0].transcript;
-        if (event.results[i].isFinal) finalTranscript += transcript + ' ';
-        else interimTranscript += transcript;
-      }
-      // document.getElementById('interim').innerHTML = interimTranscript
-      // document.getElementById('final').innerHTML = finalTranscript
-      this.props.startedListening(finalTranscript);
-    };
-    console.log('DONE LISTENING');
-  };
+			for (let i = event.resultIndex; i < event.results.length; i++) {
+				const transcript = event.results[i][0].transcript;
+				if (event.results[i].isFinal) finalTranscript += transcript + ' ';
+				else interimTranscript += transcript;
+			}
+			// document.getElementById('interim').innerHTML = interimTranscript
+			// document.getElementById('final').innerHTML = finalTranscript
+			this.setState({
+				listening: !this.state.listening
+			});
+			this.props.startedListening(finalTranscript);
+		};
+		console.log('DONE LISTENING');
+	};
 
-  render() {
-    return (
-      <div>
-        <button
-          className="btn waves-effect waves-light grey"
-          id="microphone-btn"
-          // style={button}
-          onClick={this.toggleListen}
-        />
-        <div id="interim"></div>
-        <div id="final"></div>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<button
+					className="btn waves-effect waves-light grey"
+					id="microphone-btn"
+					// style={button}
+					onClick={this.toggleListen}
+				/>
+				<div id="interim" />
+				<div id="final" />
+			</div>
+		);
+	}
 }
 
 export default InputSpeech;
